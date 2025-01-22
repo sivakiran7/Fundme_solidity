77c99ccece80a35babba47d321dfb5399d4facab
Siva Kiran <119920146+sivakiran7@users.noreply.github.com>
2025-01-22 19:05:32 +0530

    Add files via upload

---
+++ fundme.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
+
+error NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+
+    // Could we make this constant?  /* hint: no! We should make it immutable! */
+    address public immutable  i_owner;
+    uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
+
+    // constant variable used to reduce the gas cost to use the cotract
+    
+    constructor() {
+        i_owner = msg.sender;
+
+        // it is a special function called only once in a time of contract creation
+        // it is used to initialize the state variables of a smart contract
+
+        //742448 with immutable
+    }
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+    
+    function getVersion() public view returns (uint256){
+        // ETH/USD price feed address of Sepolia Network.
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+
+    
+    modifier onlyOwner {
+        // require(msg.sender == owner);
+        if (msg.sender != i_owner)  { revert NotOwner();}
+        _;  // errors are used to reduse the gas efficiency to make it to use less gas
+    }
+
+
+    
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+
+    receive() external payable {
+        fund();
+      }
+    fallback() external payable {
+        fund();
+      }
+
+/*
+things to cover
+
+1.enums
+2.events
+3.try/catch
+4.function selectors
+5.abi.encode /decode
+6.hashing
+7.yul/ assumbly
+
+
+*/
+
+}
